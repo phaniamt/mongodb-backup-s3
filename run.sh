3d94021ee4b9c8e86944c77e0613799647f2deb5
phaniamt <45986842+phaniamt@users.noreply.github.com>
2019-08-16 15:08:46 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,8 +17,6 @@ S3PATH="s3://$BUCKET/$BACKUP_FOLDER"
 [[ ( -n "${MONGODB_PASS}" ) ]] && PASS_STR=" --password ${MONGODB_PASS}"
 [[ ( -n "${MONGODB_DB}" ) ]] && DB_STR=" --db ${MONGODB_DB}"
 
-# Export AWS Credentials into env file for cron job
-printenv | sed 's/^\([a-zA-Z0-9_]*\)=\(.*\)$/export \1="\2"/g' | grep -E "^export AWS" > /root/project_env.sh
 
 echo "=> Creating backup script"
 rm -f /backup.sh
@@ -39,57 +37,10 @@ echo "=> Done"
 EOF
 chmod +x /backup.sh
 echo "=> Backup script created"
-
-echo "=> Creating restore script"
-rm -f /restore.sh
-cat <<EOF >> /restore.sh
-#!/bin/bash
-if [[( -n "\${1}" )]];then
-    RESTORE_ME=\${1}.dump.gz
-else
-    RESTORE_ME=latest.dump.gz
-fi
-S3RESTORE=${S3PATH}\${RESTORE_ME}
-aws configure set default.s3.signature_version s3v4
-echo "=> Restore database from \${RESTORE_ME}"
-if aws s3 cp \${S3RESTORE} \${RESTORE_ME} ${REGION_STR} && mongorestore --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} --drop --archive=\${RESTORE_ME} --gzip && rm \${RESTORE_ME}; then
-    echo "   Restore succeeded"
-else
-    echo "   Restore failed"
-fi
-echo "=> Done"
-EOF
-chmod +x /restore.sh
-echo "=> Restore script created"
-
-echo "=> Creating list script"
-rm -f /listbackups.sh
-cat <<EOF >> /listbackups.sh
-#!/bin/bash
-aws s3 ls ${S3PATH} \${REGION_STR}
-EOF
-chmod +x /listbackups.sh
-echo "=> List script created"
-
-ln -s /restore.sh /usr/bin/restore
 ln -s /backup.sh /usr/bin/backup
-ln -s /listbackups.sh /usr/bin/listbackups
-
-touch /mongo_backup.log
-
 if [ -n "${INIT_BACKUP}" ]; then
     echo "=> Create a backup on the startup"
     /backup.sh
 fi
 
-if [ -n "${INIT_RESTORE}" ]; then
-    echo "=> Restore store from lastest backup on startup"
-    /restore.sh
-fi
-
-if [ -z "${DISABLE_CRON}" ]; then
-    echo "${CRON_TIME} . /root/project_env.sh; /backup.sh >> /mongo_backup.log 2>&1" > /crontab.conf
-    crontab  /crontab.conf
-    echo "=> Running cron job"
-    cron && tail -f /mongo_backup.log
-fi
+exit 0
